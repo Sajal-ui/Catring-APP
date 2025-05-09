6798796df364db06c2852f49a09b0c75feb1c13e
Sajal <sp263148@gmail.com>
2025-05-09 23:45:44 +0530

    router added for event and facility.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi.responses import JSONResponse
 
 from Booking.router import router as booking_router
 from Users.router import router as users_router
+from Event.router import router as event_router
+from Facility.router import router as facility_router
 
 app = FastAPI()
 
@@ -16,3 +18,5 @@ async def get_config():
 
 app.include_router(users_router)
 app.include_router(booking_router)
+app.include_router(event_router)
+app.include_router(facility_router)
