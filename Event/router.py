88ab50c1f6b954edaecdd39c2fbbb2f8ba0676ea
Sajal <sp263148@gmail.com>
2025-05-13 00:38:02 +0530

    local mem used for running this.

--- Event/router.py
+++ Event/router.py
@@ -4,38 +4,54 @@ from fastapi import APIRouter, Depends
 from CatringApp.database import get_db
 from CatringApp.session import CustomSession
 
+Event_db = []
+
 router = APIRouter(prefix="/events", tags=["events"])
 
 @router.get("/", response_model=list[EventParser])
 async def list_events(db:CustomSession = Depends(get_db)):
-   events = db.query(Event).all()
+   # events = db.query(Event).all()
+   events = Event_db
    return events
 
 @router.post("/", response_model=EventParser)
 async def create_event(event: EventParser, db:CustomSession = Depends(get_db)):
-   event_instance = Event(display_name=event.display_name)
-   db.add(event_instance)
-   db.commit()
-   db.refresh(event_instance)
-   return event_instance
+   # event_instance = Event(display_name=event.display_name)
+   # db.add(event_instance)
+   # db.commit()
+   # db.refresh(event_instance)
+   Event_db.append(event)
+   # return event_instance
+   return event
 
 @router.get("/{id}", response_model=EventParser)
 async def get_event(id: int, db:CustomSession = Depends(get_db)):
-   event = db.query(Event).filter(Event.id == id).first()
-   return event
+   # event = db.query(Event).filter(Event.id == id).first()
+   for event in Event_db:
+      if event["id"] == id:
+         return event
+   return {"Not Found"}
    
 @router.put("/{id}", response_model=EventParser)
 async def update_event(id: int, event: EditEventParser, db:CustomSession = Depends(get_db)):
-   event_instance = db.query(Event).filter(Event.id == id).first()
-   event_instance.display_name = event.display_name
-   db.add(event_instance)
-   db.commit()
-   db.refresh(event_instance)
-   return event_instance
+   # event_instance = db.query(Event).filter(Event.id == id).first()
+   # event_instance.display_name = event.display_name
+   # db.add(event_instance)
+   # db.commit()
+   # db.refresh(event_instance)
+   for event in Event_db:
+      if event["id"] == id:
+         event["display_name"] = event.display_name
+         return event
+   return {"Not Found"}
 
 @router.delete("/{id}")
 async def delete_event(id: int, db:CustomSession = Depends(get_db)):
-   event = db.query(Event).filter(Event.id == id).first()
-   db.delete(event)
-   db.commit()
-   return {"message": "Event deleted successfully"}
+   # event = db.query(Event).filter(Event.id == id).first()
+   # db.delete(event)
+   # db.commit()
+   for event in Event_db:
+      if event["id"] == id:
+         Event_db.remove(event)
+         return {"message": "Event deleted successfully"}
+   return {"Not Found"}
--- Facility/router.py
+++ Facility/router.py
@@ -5,38 +5,58 @@ from ..database import get_db
 from .models import Facility
 from CatringApp.session import CustomSession
 
+Facility_db = []
+
 router = APIRouter(prefix="/facility", tags=["facility"])
 
 @router.get("/", response_model=list[FacilityParser])
 async def list_events(db:CustomSession = Depends(get_db)):
-   facilities = db.query(Facility).all()
-   return facilities
+   return Facility_db
+   # facilities = db.query(Facility).all()
+   # return facilities
 
 @router.post("/", response_model=FacilityParser)
 async def create_event(facility: FacilityParser, db:CustomSession = Depends(get_db)):
-   facility_instance = Facility(display_name=facility.display_name, status=facility.status)
-   db.add(facility_instance)
-   db.commit()
-   db.refresh(facility_instance)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-   return facility_instance
+   # facility_instance = Facility(display_name=facility.display_name, status=facility.status)
+   # db.add(facility_instance)
+   # db.commit()
+   # db.refresh(facility_instance)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+   # return facility_instance
+   Facility_db.append(facility)
+   return facility
 
 @router.get("/{id}", response_model=FacilityParser)
 async def get_event(id: int, db:CustomSession = Depends(get_db)):
-   facility = db.query(Facility).filter(Facility.id == id).first()
-   return facility
+   # facility = db.query(Facility).filter(Facility.id == id).first()
+   # return facility
+   for facility in Facility_db:
+      if facility["id"] == id:
+         return facility
+   return {"Not Found"}
    
 @router.put("/{id}", response_model=FacilityParser)
 async def update_event(id: int, facility: EditFacilityParser, db:CustomSession = Depends(get_db)):
-   facility_instance = db.query(Facility).filter(Facility.id == id).first()
-   facility_instance.display_name = facility.display_name
-   db.add(facility_instance)
-   db.commit()
-   db.refresh(facility_instance)
-   return facility_instance
+   # facility_instance = db.query(Facility).filter(Facility.id == id).first()
+   # facility_instance.display_name = facility.display_name
+   # db.add(facility_instance)
+   # db.commit()
+   # db.refresh(facility_instance)
+   # return facility_instance
+   for facility in Facility_db:
+      if facility["id"] == id:
+         facility["display_name"] = facility.display_name
+         facility["status"] = facility.status
+         return facility
+   return {"Not Found"}
 
 @router.delete("/{id}")
 async def delete_event(id: int, db:CustomSession = Depends(get_db)):
-   facility = db.query(Facility).filter(Facility.id == id).first()
-   db.delete(facility)
-   db.commit()
-   return {"message": "Facility deleted successfully"}
+   # facility = db.query(Facility).filter(Facility.id == id).first()
+   # db.delete(facility)
+   # db.commit()
+   # return {"message": "Facility deleted successfully"}
+   for facility in Facility_db:
+      if facility["id"] == id:
+         Facility_db.remove(facility)
+         return {"message": "Facility deleted successfully"}
+   return {"Not Found"}
